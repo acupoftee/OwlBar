de840743a361909e993e261c7c8bba1b64fbfa45
Tee <teedia31@gmail.com>
2020-02-15 08:22:17 -0600

    Add global theme and styles

---
+++ src/renderer/styles/theme.ts
@@ -0,0 +1,16 @@
+export const colors = {
+  white: "#f7f7f7",
+  liteGrey: "#8D99AE",
+  black: "#32292F",
+  orange: "#E48B00"
+};
+
+export const fonts = {
+  base:
+    '-apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif'
+};
+
+export default {
+  colors,
+  fonts
+};
