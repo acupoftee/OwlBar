1aae2c2f8a619f893faecbe808c84f55c886411d
Tee <teedia31@gmail.com>
2020-03-03 17:14:17 -0500

    Swapped to HashRouter for electron

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import configureStore from './renderer/store'
 import Standings from './renderer/containers/Standings'
