a7bb4b8cdb4548161bf43cbd2791c7c303847158
Tee <teedia31@gmail.com>
2020-03-10 17:41:32 -0400

    Fix app content render bug

--- public/electron.js
+++ public/electron.js
@@ -5,44 +5,9 @@ const path = require('path')
 const isDev = require('electron-is-dev')
 const { resolve } = require('app-root-path')
 
-
-// let win
-
-// function createWindow() {
-//   win = new BrowserWindow({
-//     icon: path.join(app.getAppPath(), 'resources/logo.png'),
-//     width: 300,
-//     height: 465,
-//     resizable: false,
-//     webPreferences: {
-//       nodeIntegration: true
-//     }
-//   })
-
-//   win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
-
-//   win.on('closed', () => {
-//     win = null
-//   })
-// }
-
-// app.on('ready', createWindow)
-
-// app.on('window-all-closed', () => {
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// app.on('activate', () => {
-//   if (win === null) {
-//     createWindow()
-//   }
-// })
-
-
 const menubarApp = menubar({
-  icon: path.join(app.getAppPath(), 'resources/logo.png'),
+  index: 'file://' + path.join(__dirname, 'index.html'),
+  icon: path.join(app.getAppPath(), 'src/resources/logo.png'),
   browserWindow: {
     width: 300,
     height: 465,
@@ -56,13 +21,5 @@ const menubarApp = menubar({
 })
 
 menubarApp.on('ready', async () => {
-  const prodPath = format({
-    pathname: resolve('../build/index.html'),
-    protocol: 'file:',
-    slashes: true,
-  });
-  console.log('production-path', prodPath)
-  console.log('dirname', __dirname)
-  menubarApp.window.loadURL(isDev ? 'http://localhost:3000' : prodPath)
   console.log('app is ready')
 })
