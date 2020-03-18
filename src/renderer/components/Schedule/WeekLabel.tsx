2c8f9064bb293400276ffb1f3b536bfaf2c1ca8c
Tee <teedia31@gmail.com>
2020-03-18 16:17:29 -0400

    Remove fragments

--- src/renderer/components/Schedule/WeekLabel.tsx
+++ src/renderer/components/Schedule/WeekLabel.tsx
@@ -1,20 +1,18 @@
 import React from 'react'
 
 const WeekLabel = ({ week }: { week: number }) => (
-  <>
-    <p
-      style={{
-        backgroundColor: 'black',
-        color: 'white',
-        padding: '4px',
-        width: '100vw',
-        marginTop: '30px',
-        textAlign: 'center',
-        marginBottom: '0',
-        borderBottom: '2px solid gray',
-        borderTop: '2px solid gray',
-      }}
-    >{`Week ${week}`}</p>
-  </>
+  <p
+    style={{
+      backgroundColor: 'black',
+      color: 'white',
+      padding: '4px',
+      width: '100vw',
+      marginTop: '30px',
+      textAlign: 'center',
+      marginBottom: '0',
+      borderBottom: '2px solid gray',
+      borderTop: '2px solid gray',
+    }}
+  >{`Week ${week}`}</p>
 )
 export default WeekLabel
