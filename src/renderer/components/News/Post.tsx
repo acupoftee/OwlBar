a693d63cc18896be08f90c5cee8d996b0d1b5ee6
Tee <teedia31@gmail.com>
2020-03-18 15:35:02 -0400

    Remove unused import

--- src/renderer/components/News/Post.tsx
+++ src/renderer/components/News/Post.tsx
@@ -5,8 +5,6 @@ import styled from 'styled-components'
 
 import { Flex } from 'antd-mobile'
 
-import { colors } from '../../styles/theme'
-
 const Wrapper = styled(Flex)`
   width: 100vw;
   height: auto;
